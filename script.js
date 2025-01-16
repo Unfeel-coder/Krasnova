3c93991707448cf7ab7d46a7ac28d0270530d512
Unfeel-coder <galex.ind@gmail.com>
2025-01-16 14:30:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-  const data = {
-    name: e.target.querySelector('#name').value,
-    surname: e.target.querySelector('#surname').value,
-    patronymic: e.target.querySelector('#patronymic').value,
-    birthdate: e.target.querySelector('#birthdate').value,
-    policy_number: e.target.querySelector('#policy_number').value,
-    policy_amount: e.target.querySelector('#policy_amount').value,
-    term: e.target.querySelector('#term').value,
-  };
+  const remainingMortgageAmount = document.querySelector('#remaining-mortgage-amount').value;
+  const propertyType = document.querySelector('#property-type').value;
+  const gender = document.querySelector('#gender').value;
 
-  fetch('/submit-policy', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+  // Здесь можно сохранить данные на сервер или выполнить другие действия с введенной информацией.
+  console.log({ remainingMortgageAmount, propertyType, gender });
 });
